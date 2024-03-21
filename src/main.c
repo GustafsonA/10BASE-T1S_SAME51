e7f9eb31b6c409cfacef53ccf7bda1322120383c
GustafsonA <agustafson@hbkworld.com>
2024-03-21 16:21:32 +0100

    1 commit (initialize)

---
+++ src/main.c
@@ -0,0 +1,64 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+#include "driver/lan865x/src/dynamic/tc6/tc6.h"
+#include "tcpip_manager_control.h"
+
+#include ""
+
+#define T1S_PLCA_NODE_ID            (BOARD_INSTANCE)
+#define FIRMWARE_VERSION            TC6_LIB_VER_STRING
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
